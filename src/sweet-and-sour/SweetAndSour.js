cb4187c5ec47a1c721baf91c4d238717eaef8ee6
ryannolanco <ryannolanco@gmail.com>
2024-04-29 17:53:29 -0600

    added board for game

--- src/sweet-and-sour/SweetAndSour.js
+++ src/sweet-and-sour/SweetAndSour.js
@@ -1,23 +1,40 @@
+import './sweet-and-sour.css';
+
+import { useEffect, useState } from 'react';
+
 const width = 8;
 const candyColors = ['blue', 'green', 'orange', 'purple', 'red', 'yellow'];
 
-
-
 const SweetAndSour = () => {
+	const [currentColorArrangment, setCurrentColorArrangment] = useState([]);
 
-  const createBoard = () => {
-      
-        const randomNumber = Math.floor(Math.random() * candyColors.length)
-        const randomColor = candyColors[randomNumber]
-        
-  }
+	const createBoard = () => {
+		const randomColorArrangment = [];
+		for (let i = 0; i < width * width; i++) {
+			const randomColor =
+				candyColors[Math.floor(Math.random() * candyColors.length)];
+			randomColorArrangment.push(randomColor);
+		}
+		setCurrentColorArrangment(randomColorArrangment);
+	};
+
+	useEffect(() => {
+		createBoard();
+	}, []);
 
 	return (
-    <>
-    <h1>Sweet and Sour</h1>
-    <button onClick={createBoard}>Random Number</button>
-    </>
-  )
+		<div className="sas-app">
+			<div className="game">
+				{currentColorArrangment.map((candyColor, index) => (
+					<img
+						alt="candy color"
+						key={index}
+						style={{ backgroundColor: candyColor }}
+					/>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default SweetAndSour;
